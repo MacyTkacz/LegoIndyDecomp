b1e0524ebea9f89839a79c958f29c8673267af1b
Macy Tkacz <macytkacz@gmail.com>
2026-06-12 05:14:09 -0500

    added filesystem abstraction header

---
+++ LegoIndyDecomp/compat/filesystem.h
@@ -0,0 +1,63 @@
+#ifndef LEGOINDY_FILESYSTEM_H
+#define LEGOINDY_FILESYSTEM_H
+
+#include <cstdint>
+#include <memory>
+
+namespace FileSystem {
+
+bool CreateDirectory(const char* path);
+bool Exists(const char* path);
+
+enum KnownPath { DOCUMENTS, LOCAL_DATA };
+const char* GetKnownPath( KnownPath path );
+
+enum FilePosition { CURRENT, START, END };
+enum FileAccessType { READ, WRITE };
+enum FileShareType { NONE, READ };
+enum FileCreateMode { CREATE_NEW=1, CREATE_ALWAYS, OPEN_EXISTING, OPEN_ALWAYS, TRUNCATE_EXISTING };
+enum FileAttribute { NORMAL=7 };
+class File {
+public:
+
+    bool SetPointer( FilePosition position );
+    bool SetPointer( uint64_t position );
+    bool SetEOF();
+    bool Save();
+    bool Read( void* dest, uint64_t bytesToRead, uint64_t* bytesRead );
+    bool Write( void* source, uint64_t bytesToWrite, uint64_t* bytesWritten );
+
+    // in win32, these functions all use info from GetFileAttributesExA
+    uint64_t GetSize();
+    uint64_t GetAttributes();
+    uint64_t GetCreationTime();
+    uint64_t GetLastAccessTime();
+    uint64_t GetLastWriteTime();
+
+private:
+
+    uint64_t pointer;
+    uint64_t eof;
+    FileAccessType accessType;
+    FileShareType shareType;
+
+};
+// creates a new / retrieves an existing file
+std::shared_ptr<File> GetFile( const char* path, FileAccessType accessType, FileShareType shareType, FileCreateMode createMode, uint64_t attributes );
+
+class Search {
+public:
+    bool FindNext();
+    std::shared_ptr<File> Get();
+private:
+    const char searchPath[1024];
+    std::shared_ptr<File> match;
+};
+std::unique_ptr<Search> FindFile( const char* searchPath );
+
+bool MoveFile( const char* existingPath, const char* newPath );
+bool DeleteFile( const char* path );
+
+}; // namespace FileSystem
+
+#endif // LEGOINDY_FILESYSTEM_H
