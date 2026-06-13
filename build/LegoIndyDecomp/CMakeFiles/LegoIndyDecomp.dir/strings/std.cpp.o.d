64e690112c4b56f3b5f07cd99c986cf87370896e
Macy Tkacz <macytkacz@gmail.com>
2026-06-12 22:31:43 -0500

    removed build folder

--- build/LegoIndyDecomp/CMakeFiles/LegoIndyDecomp.dir/strings/std.cpp.o.d
+++
@@ -1,4 +0,0 @@
-LegoIndyDecomp/CMakeFiles/LegoIndyDecomp.dir/strings/std.cpp.o: \
- /home/macytkacz/Documents/LegoIndyDecomp/LegoIndyDecomp/strings/std.cpp \
- /usr/include/stdc-predef.h \
- /home/macytkacz/Documents/LegoIndyDecomp/LegoIndyDecomp/strings/std.h
